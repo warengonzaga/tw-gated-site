470ed69fc0833c2b5ab5b9dca669fb2514be83c6
Waren Gonzaga <github@warengonzaga.com>
2022-08-13 00:53:04 +0800

    initial commit

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const contractAddress = "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF";
+export const contractAddress = "0x70BdeE351367913d97baC6f8b7A7f717A7873f2E";
 
 // Replace this with your domain name (or any other name you want)
-export const domainName = "example.org";
+export const domainName = "nft-gated.warengonzaga.com";
--- pages/login.jsx
+++ pages/login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
 
   // For user to claim an NFT to then view the restricted content
   const editionDropContract = useEditionDrop(
-    "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF" // replace this with your contract address
+    "0x70BdeE351367913d97baC6f8b7A7f717A7873f2E" // replace this with your contract address
   );
 
   // Hook to claim NFTs from the NFT drop (to allow users to claim and *then* view the restricted content)
